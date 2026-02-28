3c57aaa8bcea37591c556b073234d356b7c18bb3
Victor200524 <victor.donaire@hotmail.com>
2026-02-27 21:46:30 -0300

    First Commit

---
+++ autoflex-backend/src/main/java/com/autoflex/AutoflexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoflexBackendApplication.class, args);
+    }
+
+}
